ed6f3189bfae2362cac7c9d27cc1f9905a6eaf0d
Joey Reid <botanize@users.noreply.github.com>
2023-01-18 14:00:46 -0600

    reimplement head block

--- Formula/prime_server.rb
+++ Formula/prime_server.rb
@@ -2,10 +2,13 @@ class PrimeServer < Formula
   desc "non-blocking (web)server API for distributed computing and SOA based on zeromq"
   homepage "https://github.com/kevinkreiser/prime_server/"
   license "BSD-2-Clause"
-  url "https://github.com/kevinkreiser/prime_server.git", branch: "master"
-  # url "https://github.com/kevinkreiser/prime_server/archive/refs/tags/0.7.0.tar.gz"
-  # version "0.7.0"
-  # sha256 "4c540c351cec29c19229b26fb0db1ba5dfba719dd0b4e029198da6a936950107"
+  url "https://github.com/kevinkreiser/prime_server/archive/refs/tags/0.7.0.tar.gz"
+  version "0.7.0"
+  sha256 "4c540c351cec29c19229b26fb0db1ba5dfba719dd0b4e029198da6a936950107"
+
+  head do
+    url "https://github.com/kevinkreiser/prime_server.git", branch: "master"
+  end
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
